d18c4d7a0dafca543be51be1e2de59e964c1af71
crmora <crmora@est.utn.ac.cr>
2026-07-26 14:29:07 -0600

    Se crea Cronometro
    se abjunta clase Cronometro y sus respectivos metodos y su constructor

---
+++ src/Modelo/Cronometro.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author UTN
+ */
+public class Cronometro {
+     // Atributos
+    private long tiempoInicio;
+    private long tiempoFin;
+    private boolean ejecutando;
+
+    
+    public Cronometro() {
+        this.tiempoInicio = 0;
+        this.tiempoFin = 0;
+        this.ejecutando = false;
+    }
+
+  
+    public Cronometro(long tiempoInicio, long tiempoFin, boolean ejecutando) {
+        this.tiempoInicio = tiempoInicio;
+        this.tiempoFin = tiempoFin;
+        this.ejecutando = ejecutando;
+    }
+
+    // Getters y Setters
+
+    public long getTiempoInicio() {
+        return tiempoInicio;
+    }
+
+    public void setTiempoInicio(long tiempoInicio) {
+        this.tiempoInicio = tiempoInicio;
+    }
+
+    public long getTiempoFin() {
+        return tiempoFin;
+    }
+
+    public void setTiempoFin(long tiempoFin) {
+        this.tiempoFin = tiempoFin;
+    }
+
+    public boolean isEjecutando() {
+        return ejecutando;
+    }
+
+    public void setEjecutando(boolean ejecutando) {
+        this.ejecutando = ejecutando;
+    }
+}
+
+
