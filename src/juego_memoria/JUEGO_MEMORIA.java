bcae8c6138a60bb58ed747e0c21ebbac877fead3
jxsecitooo <joseandresalvarez1009@gmail.com>
2026-07-28 14:03:41 -0600

    Metodo Main
    Se crea el metodo main en paquete juego memoria

--- src/juego_memoria/JUEGO_MEMORIA.java
+++ src/juego_memoria/JUEGO_MEMORIA.java
@@ -14,7 +14,9 @@ public class JUEGO_MEMORIA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        MenuJuego Menu = new MenuJuego();
+        Menu.setVisible(true);
+        Menu.setLocationRelativeTo(null);
     }
     
 }
